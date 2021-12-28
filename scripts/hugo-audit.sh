dfd3fa5555a88c75bf9ca0e41a126500d762629f
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2021-12-27 19:58:53 -0500

    Fix Hugo resource dir definition
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- scripts/hugo-audit.sh
+++ scripts/hugo-audit.sh
@@ -2,7 +2,7 @@
 
 set -o pipefail
 
-if HUGO_MINIFY_TDEWOLFF_HTML_KEEPCOMMENTS=true HUGO_ENABLEMISSINGTRANSLATIONPLACEHOLDERS=true hugo --gc --cleanDestinationDir  --destination $(pwd)/public --source $(pwd)/exampleSite && \
+if HUGO_MINIFY_TDEWOLFF_HTML_KEEPCOMMENTS=true HUGO_ENABLEMISSINGTRANSLATIONPLACEHOLDERS=true HUGO_RESOURCEDIR=$(pwd)/resources hugo --gc --cleanDestinationDir  --destination $(pwd)/public --source $(pwd)/exampleSite && \
     grep -iIvrnE "grep(.+(-- raw HTML omitted --|ZgotmplZ|hahahugo|\\\[i18n\\\])+)" public/ | \
     grep -iIoE "<\!-- raw HTML omitted -->|ZgotmplZ|hahahugo|\[i18n\]"
 then
