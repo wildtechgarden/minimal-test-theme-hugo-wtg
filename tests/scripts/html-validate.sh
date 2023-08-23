438163a2f67b78e21a41416903170590dd11217d
Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>
2023-08-23 13:02:38 -0400

    Replace node html-validator with nu w3c validator
    Signed-off-by: Daniel F. Dickinson <dfdpublic@wildtechgarden.ca>

--- tests/scripts/html-validate.sh
+++ tests/scripts/html-validate.sh
@@ -5,14 +5,14 @@ set -o pipefail
 
 SITEROOT="$(pwd)"
 
-#if [ -d exampleSite ]; then
-#    SITESRC=exampleSite/
-#fi
-
-if echo y | npx html-validate -c .htmlvalidate.json \
-	"${SITEROOT}"/${SITESRC}public/** 2>&1 | tee html-validate.log; then
-	echo "ok"
-	exit 0
+if java -jar ~/jar/vnu.jar --Werror $(find public -name '*.html') | tee html-validate.log; then
+	if java -jar ~/jar/vnu.jar --Werror --css $(find public -name '*.css') | tee html-validate.log; then
+		echo "ok"
+		exit 0
+	else
+		echo "not ok"
+		exit 1
+	fi
 else
 	echo "not ok"
 	exit 1
